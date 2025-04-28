70f2305ec379ed43a04a9fe799caeae0d731795a
piettrasattolo <piettrabackup@gmail.com>
2025-04-28 14:55:47 -0300

    Questão 2 – Condicional If/Else If/Else

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 //Questão 1
 
-var tarefaConcluida = false;
+//var tarefaConcluida = false;
+//
+//if (tarefaConcluida === true) {
+//  console.log("A tarefa está concluída!");
+//
+//} else {
+//  console.log("A tarefa não foi concluída.");
+//}
 
-if (tarefaConcluida === true) {
-  console.log("A tarefa está concluída!");
+//------------------------------
+
+//Questão 2
+
+var prioridade = 2;
+
+if (prioridade === 1) {
+  console.log("Prioridade Baixa");
+
+} else if (prioridade === 2) {
+  console.log("Prioridade Média");
+
+} else if (prioridade === 3) {
+  console.log("Prioridade Alta");
 
 } else {
-  console.log("A tarefa não foi concluída.");
+  console.log("Nenhuma Prioridade");
 }
