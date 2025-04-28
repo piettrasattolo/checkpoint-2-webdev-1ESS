c9f2f617f40fd23732bbd28426f6ca5fdea38bff
piettrasattolo <piettrabackup@gmail.com>
2025-04-28 15:00:30 -0300

    Questão 4 – Loop For

--- script.js
+++ script.js
@@ -32,40 +32,48 @@
 
 //Questão 3
 
-let diaSemana = new Date("04-20-2025").getDay();
+//let diaSemana = new Date("04-20-2025").getDay();
+//
+//console.log(diaSemana);
+//
+//switch (diaSemana) {
+//  case 0:
+//    console.log("Domingo");
+//    break;
+//
+//  case 1:
+//    console.log("Segunda-feira");
+//    break;
+//
+//  case 2:
+//    console.log("Terça-feira");
+//    break;
+//
+//  case 3:
+//    console.log("Quarta-feira");
+//    break;
+//
+//  case 4:
+//    console.log("Quinta-feira");
+//    break;
+//
+//  case 5:
+//    console.log("Sexta-feira");
+//    break;
+//
+//  case 6:
+//    console.log("Sábado");
+//    break;
+//
+//  default:
+//    console.log("Dia Inválido");
+//    break;
+//}
 
-console.log(diaSemana);
+//------------------------------
 
-switch (diaSemana) {
-  case 0:
-    console.log("Domingo");
-    break;
+//Questão 4
 
-  case 1:
-    console.log("Segunda-feira");
-    break;
-
-  case 2:
-    console.log("Terça-feira");
-    break;
-
-  case 3:
-    console.log("Quarta-feira");
-    break;
-
-  case 4:
-    console.log("Quinta-feira");
-    break;
-
-  case 5:
-    console.log("Sexta-feira");
-    break;
-
-  case 6:
-    console.log("Sábado");
-    break;
-
-  default:
-    console.log("Dia Inválido");
-    break;
+for (var i = 0; i <= 9; i++){
+    console.log(i)
 }
