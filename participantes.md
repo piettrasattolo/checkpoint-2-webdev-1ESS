77a00599db0eab28675d44a7015b5ab45bad4c44
piettrasattolo <piettrabackup@gmail.com>
2025-04-28 14:52:33 -0300

    Questão 1 – Condicional If

---
+++ participantes.md
@@ -0,0 +1,3 @@
+Henrique Farah Brigo rm-563323
+Raul Laguna Arboleya Andrade rm-562684
+Piettra Boschini Sattolo rm-562538
---
+++ script.js
@@ -0,0 +1,10 @@
+//Questão 1
+
+var tarefaConcluida = false;
+
+if (tarefaConcluida === true) {
+  console.log("A tarefa está concluída!");
+
+} else {
+  console.log("A tarefa não foi concluída.");
+}
